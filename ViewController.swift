6e4a4f9597640d3996ed9864ce8e8e14ecc7c381
gdsanders <gd@digitalequity.com>
2015-08-13 16:40:50 -0500

    background image; text color change

--- ViewController.swift
+++ ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!)
     }
 
     override func didReceiveMemoryWarning() {
